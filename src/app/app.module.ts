5d8cca8d9bb8701c97dd20dd2540fe548d77629c
Lokalni CroRIS4 <lu-croris4@srce.hr>
2021-12-07 18:27:54 +0100

    Basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgxPainlessFormModule } from 'ngx-painless-form';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { LandingComponent } from './component/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
