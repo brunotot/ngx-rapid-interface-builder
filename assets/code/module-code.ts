4bc455b7773d3e79125d79651bdf89cf57c5108c
Lokalni CroRIS4 <lu-croris4@srce.hr>
2021-12-09 16:49:51 +0100

    Auto-generated commit [ci skip]

---
+++ assets/code/module-code.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { NgxPainlessFormModule } from 'ngx-painless-form';
+@NgModule({
+  // ...
+  imports: [
+    NgxPainlessFormModule
+  ]
+})
+export class AppModule { }
